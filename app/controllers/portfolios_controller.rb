58742de7c151ad8649541d45d59310fc6478270f
jie7206 <1524556052@qq.com>
2022-09-29 16:38:19 +0800

    Integrated new and create actions for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,7 +1,29 @@
 class PortfoliosController < ApplicationController
 
   def index
-    @portfolio_items = Portfolio.all
+    @portfolios = Portfolio.all
+  end
+
+  def new
+    @portfolio = Portfolio.new
+  end
+
+  def create
+    @portfolio = Portfolio.new(portfolio_params)
+
+    respond_to do |format|
+      if @portfolio.save
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now saved.'}
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title,:subtitle,:body)
   end
 
 end
